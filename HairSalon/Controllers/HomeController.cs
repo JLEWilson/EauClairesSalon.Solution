e94a389a0849bc638d172e5f8bec42e42f87ee76
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-26 12:00:46 -0900

    add reference to clients for styling purposes

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly HairSalonContext _db;
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      List<Client> model = _db.Clients.ToList(); //This is all just so I only show the link to view all clients if there are in fact clients.
+      return View(model);
     }
   }
 }
